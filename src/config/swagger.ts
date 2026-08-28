40b69c850d3ded12c037c75491454524f25d5445
kidist311 <alemayehukidist09@gmail.com>
2026-08-28 11:12:33 +0300

    fix: use relative API URL in Swagger

--- src/config/swagger.ts
+++ src/config/swagger.ts
@@ -13,7 +13,7 @@ const swaggerOptions: swaggerJSDoc.Options = {
 
     servers: [
       {
-        url: "http://localhost:5000/api/v1",
+        url: "/api/v1",
         description: "Local development server",
       },
     ],
